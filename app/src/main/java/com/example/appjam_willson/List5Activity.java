c43ce23910cf2de36207ff0790f69d198baec63a
dohan <adfilm2@naver.com>
2019-07-01 07:01:29 +0900

    add text_color_selector.xml kill bug

--- app/src/main/java/com/example/appjam_willson/List5Activity.java
+++ app/src/main/java/com/example/appjam_willson/List5Activity.java
@@ -2,7 +2,10 @@ package com.example.appjam_willson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class List5Activity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class List5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list5);
+
+        RadioButton man = (RadioButton)findViewById(R.id.man_btn);
+        RadioButton woman = (RadioButton)findViewById(R.id.woman_btn);
+        RadioButton all = (RadioButton)findViewById(R.id.all_btn);
+
+
+
     }
+
+
 }
