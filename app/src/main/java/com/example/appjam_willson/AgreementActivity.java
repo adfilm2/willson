740d9e428b228d77d5b9d20ca4e3c6c922c94f3e
dohan <adfilm2@naver.com>
2019-07-01 00:15:22 +0900

    agreement page btn

--- app/src/main/java/com/example/appjam_willson/AgreementActivity.java
+++ app/src/main/java/com/example/appjam_willson/AgreementActivity.java
@@ -3,12 +3,20 @@ package com.example.appjam_willson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
-public class MainActivity extends AppCompatActivity {
+public class AgreementActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.check1);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.check2);
+
+
+
     }
 }
