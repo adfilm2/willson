62e5b13137b33038dab4c71285e9f37db593d7dd
dohan <adfilm2@naver.com>
2019-07-01 23:28:09 +0900

    edit activity6 layout and next btn

--- app/src/main/java/com/example/appjam_willson/List6Activity.java
+++ app/src/main/java/com/example/appjam_willson/List6Activity.java
@@ -3,6 +3,8 @@ package com.example.appjam_willson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class List6Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class List6Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list6);
+    }
+
+    public void thicker(View view) {
+        EditText text = (EditText)view;
+        text.setBackgroundResource(R.drawable.rounded_corner_thick);
     }
 }
