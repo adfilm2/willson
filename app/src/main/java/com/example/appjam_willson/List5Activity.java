2687c6ed689db37db797d3c4a464444f60e082ee
dohan <adfilm2@naver.com>
2019-07-01 19:59:39 +0900

    add character choice add next btn

--- app/src/main/java/com/example/appjam_willson/List5Activity.java
+++ app/src/main/java/com/example/appjam_willson/List5Activity.java
@@ -4,19 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
 public class List5Activity extends AppCompatActivity {
+
+    public int check_num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list5);
 
-        RadioButton man = (RadioButton)findViewById(R.id.man_btn);
-        RadioButton woman = (RadioButton)findViewById(R.id.woman_btn);
-        RadioButton all = (RadioButton)findViewById(R.id.all_btn);
+    }
+
+    public void char_check(View view){
+        CheckBox checkBox = (CheckBox)view;
+
+                if (check_num < 3) {
+                    if (!checkBox.isChecked()) {
+                        checkBox.setChecked(false);
+                        check_num -= 1;
+                        if(check_num<=0) check_num=0;
+
+                    } else {
+                        checkBox.setChecked(true);
+                        check_num += 1;
+                        if(check_num>3) check_num =3;
+
+                    }
+                } else {
+                    if (checkBox.isChecked()) {
+                        checkBox.setChecked(false);
+
+                    }
+                    else{
+                        checkBox.setChecked(false);
+                        check_num -= 1;
+
+                    }
+                }
+
 
 
 
@@ -24,3 +56,7 @@ public class List5Activity extends AppCompatActivity {
 
 
 }
+
+
+
+
