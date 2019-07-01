655087955e24dccf2fb9bcdccf6c13f63161efd6
yj <pl3434on@gmail.com>
2019-07-01 20:12:50 +0900

    make list_toolbar make list1activity toolbar_list in activity_list1.xml add build.gradle (app) to problem solved add progress bar shadow edit toolbar background color edit toolbar margin bottom add text selector kill bug edit listnext_btn_selector add shadow edit activity_list1 layout edit colors name edit activity_list1 nextbtn width edit branchtest edit list1 activity.xml create radio button create text view create List1Activity setOnClickListeners add edittext, textview

--- app/src/main/java/com/example/appjam_willson/List1Activity.java
+++ app/src/main/java/com/example/appjam_willson/List1Activity.java
@@ -2,29 +2,43 @@ package com.example.appjam_willson;
 
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class List1Activity extends AppCompatActivity implements OnClickListener {
 
-    private RadioGroup list1_radioGroup1;
-    private RadioGroup list1_radioGroup2;
+    RadioGroup list1_radioGroup1;
+    RadioGroup list1_radioGroup2;
 
-    private Button list1_nextbtn;
+    Button list1_nextbtn;
+    TextView custom_text;
+    EditText custom_edit_text;
+    LinearLayout usercustom_layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +54,34 @@ public class List1Activity extends AppCompatActivity implements OnClickListener 
 
         list1_nextbtn = (Button) findViewById(R.id.list1_btn_next);
         list1_nextbtn.setOnClickListener(this);
+
+        custom_text = (TextView)findViewById(R.id.list1_btn_usercustom);
+        custom_text.setOnClickListener(new custom_btn_listener());
+
+        custom_edit_text = (EditText)findViewById(R.id.list1_usercustom_edittext);
+        custom_edit_text.setOnClickListener(new custom_edit_Clicklistener());
+        custom_edit_text.setOnKeyListener(new custom_edit_listener());
+
+        usercustom_layout = (LinearLayout)findViewById(R.id.list1_btn_usercustom_layout);
     }
 
     private OnCheckedChangeListener radioGroup_listener1 = new OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             if (checkedId != -1) {
+                hidekeyboard(custom_edit_text);
                 list1_radioGroup2.setOnCheckedChangeListener(null);
                 list1_radioGroup2.clearCheck();
                 list1_radioGroup2.setOnCheckedChangeListener(radioGroup_listener2);
+                usercustom_layout.setBackgroundResource(R.drawable.list_btns_selector);
+                int backcolor = getResources().getColor(R.color.lightPurple);
+                custom_edit_text.setTextColor(backcolor);
+                String title;
+                title = custom_edit_text.getText().toString();
+                if(title.getBytes().length <= 0) {
+                    custom_text.setVisibility(View.VISIBLE);
+                    custom_edit_text.setVisibility(View.INVISIBLE);
+                }
             }
         }
     };
@@ -57,30 +90,84 @@ public class List1Activity extends AppCompatActivity implements OnClickListener 
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             if(checkedId != -1) {
+                hidekeyboard(custom_edit_text);
                 list1_radioGroup1.setOnCheckedChangeListener(null);
                 list1_radioGroup1.clearCheck();
                 list1_radioGroup1.setOnCheckedChangeListener(radioGroup_listener1);
+                usercustom_layout.setBackgroundResource(R.drawable.list_btns_selector);
+                int backcolor = getResources().getColor(R.color.lightPurple);
+                custom_edit_text.setTextColor(backcolor);
+                String title;
+                title = custom_edit_text.getText().toString();
+                if(title.getBytes().length <= 0) {
+                    custom_text.setVisibility(View.VISIBLE);
+                    custom_edit_text.setVisibility(View.INVISIBLE);
+                }
             }
         }
     };
 
-
     @SuppressLint("ResourceType")
     @Override
     public void onClick(View v) {
-        /*switch(v.getId()) {
-            case R.id.list1_btn_next :
-            if (list1_radioGroup1.getCheckedRadioButtonId() > 0) {
-                View radioButton = list1_radioGroup1.findViewById(list1_radioGroup1.getCheckedRadioButtonId());
-                int radioId = list1_radioGroup1.indexOfChild(radioButton);
-                RadioButton btn = (RadioButton) list1_radioGroup1.getChildAt(radioId);
-            }
-            else if(list1_radioGroup2.getCheckedRadioButtonId() > 0) {
-                View radioButton = list1_radioGroup2.findViewById(list1_radioGroup2.getCheckedRadioButtonId());
-                int radioId = list1_radioGroup2.indexOfChild(radioButton);
-                RadioButton btn = (RadioButton) list1_radioGroup2.getChildAt(radioId);
-            }
-        }*/
 
     }
+
+    class custom_btn_listener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            list1_radioGroup1.setOnCheckedChangeListener(null);
+            list1_radioGroup1.clearCheck();
+            list1_radioGroup1.setOnCheckedChangeListener(radioGroup_listener1);
+            list1_radioGroup2.setOnCheckedChangeListener(null);
+            list1_radioGroup2.clearCheck();
+            list1_radioGroup2.setOnCheckedChangeListener(radioGroup_listener2);
+            custom_text.setVisibility(View.INVISIBLE);
+            custom_edit_text.setVisibility(View.VISIBLE);
+            int backcolor = getResources().getColor(R.color.white);
+            usercustom_layout.setBackgroundResource(R.drawable.list_btns_selected);
+            custom_edit_text.setTextColor(backcolor);
+        }
+    }
+
+    class custom_edit_Clicklistener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            list1_radioGroup1.setOnCheckedChangeListener(null);
+            list1_radioGroup1.clearCheck();
+            list1_radioGroup1.setOnCheckedChangeListener(radioGroup_listener1);
+            list1_radioGroup2.setOnCheckedChangeListener(null);
+            list1_radioGroup2.clearCheck();
+            list1_radioGroup2.setOnCheckedChangeListener(radioGroup_listener2);
+            custom_text.setVisibility(View.INVISIBLE);
+            custom_edit_text.setVisibility(View.VISIBLE);
+            usercustom_layout.setBackgroundResource(R.drawable.list_btns_selector);
+            int backcolor = getResources().getColor(R.color.white);
+            usercustom_layout.setBackgroundResource(R.drawable.list_btns_selected);
+            custom_edit_text.setTextColor(backcolor);
+        }
+    }
+
+    class custom_edit_listener implements View.OnKeyListener {
+
+        @Override
+        public boolean onKey(View view, int i, KeyEvent keyEvent) {
+
+            switch (i) {
+                case KeyEvent.KEYCODE_ENTER :
+                    int backcolor = getResources().getColor(R.color.white);
+                    custom_edit_text.setTextColor(backcolor);
+                    hidekeyboard(custom_edit_text);
+            }
+            return false;
+        }
+    }
+
+    private void hidekeyboard(EditText edit) {
+        InputMethodManager input = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        input.hideSoftInputFromWindow(edit.getWindowToken(), 0);
+    }
+
+
+
 }
