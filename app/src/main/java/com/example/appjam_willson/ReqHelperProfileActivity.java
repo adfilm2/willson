3b51067113ae6135c89b9645e1a1709ad395f6c4
dohan <adfilm2@naver.com>
2019-07-03 04:42:08 +0900

    add helperProfile

---
+++ app/src/main/java/com/example/appjam_willson/ReqHelperProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.appjam_willson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReqHelperProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_req_helper_profile);
+    }
+}
