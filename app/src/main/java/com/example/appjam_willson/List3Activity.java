a186a0c31825ef4e4cddd50969a5794f024fa8b8
dohan <adfilm2@naver.com>
2019-07-03 00:12:50 +0900

    add radio_btn layout in list7

--- app/src/main/java/com/example/appjam_willson/List3Activity.java
+++ app/src/main/java/com/example/appjam_willson/List3Activity.java
@@ -56,24 +56,24 @@ public class List3Activity extends AppCompatActivity {
         //r_btn2.setOnClickListener(radioButtonClickListener);
 
         //라디오 그룹 설정
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
+//        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+//        radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
     }
 
 
-
-    //라디오 그룹 클릭 리스너
-    RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
-
-        @Override
-        public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-            btnNext.setEnabled(true);
-
-            if (i == R.id.rg_btn1) {
-                Toast.makeText(List3Activity.this, "라디오 그룹 버튼1 눌렸습니다.", Toast.LENGTH_SHORT).show();
-            } else if (i == R.id.rg_btn2) {
-                Toast.makeText(List3Activity.this, "라디오 그룹 버튼2 눌렸습니다.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
+//
+//    //라디오 그룹 클릭 리스너
+//    RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+//
+//        @Override
+//        public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+//            btnNext.setEnabled(true);
+//
+//            if (i == R.id.rg_btn1) {
+//                Toast.makeText(List3Activity.this, "라디오 그룹 버튼1 눌렸습니다.", Toast.LENGTH_SHORT).show();
+//            } else if (i == R.id.rg_btn2) {
+//                Toast.makeText(List3Activity.this, "라디오 그룹 버튼2 눌렸습니다.", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    };
 }
