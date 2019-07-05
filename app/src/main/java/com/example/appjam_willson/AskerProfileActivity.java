ed55fe122c210af65a5e9be59a3acace7336d225
dohan <adfilm2@naver.com>
2019-07-05 19:49:38 +0900

    add activity_asker_profile add floatingButton

---
+++ app/src/main/java/com/example/appjam_willson/AskerProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.appjam_willson;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AskerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asker_profile);
+    }
+}
