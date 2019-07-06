e64f8c9310d33ae5bb77b03b2a03b47e5c88c4c8
박지영 <batzerohi@gmail.com>
2019-07-06 17:12:16 +0900

    Helper profile edit activity

---
+++ app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivity0.kt
@@ -0,0 +1,13 @@
+package com.example.appjam_willson.HelperProfileEdit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appjam_willson.R
+
+class HelperProfileEditActivity0 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helper_profile_edit0)
+    }
+}
