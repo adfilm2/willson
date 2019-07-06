07ccdbb1d90fae911316f87dec3c4a6c905fb7a3
박지영 <batzerohi@gmail.com>
2019-07-06 23:28:54 +0900

    Merge branch 'master' of https://github.com/adfilm2/willson
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/appjam_willson/HelperSignUpActivity/HelperSignUpActivityStart.kt
@@ -0,0 +1,14 @@
+package com.example.appjam_willson.HelperSignUpActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appjam_willson.R
+
+class HelperSignUpActivityStart : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helper_sign_up_start)
+    }
+
+}
