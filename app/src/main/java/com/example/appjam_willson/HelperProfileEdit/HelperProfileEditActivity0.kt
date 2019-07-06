56ad6d2a37e835b40688aa4782a37a1f0daa673d
박지영 <batzerohi@gmail.com>
2019-07-06 22:19:37 +0900

    Merge branch 'master' of https://github.com/adfilm2/willson
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivity0.kt
+++ app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivity0.kt
@@ -9,5 +9,8 @@ class HelperProfileEditActivity0 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_helper_profile_edit0)
+
+
+
     }
 }
