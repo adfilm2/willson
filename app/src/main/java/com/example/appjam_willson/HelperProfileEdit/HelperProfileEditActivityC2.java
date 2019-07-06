bc2f373da244f27738d72f418c7c193fc8bdecc1
박지영 <batzerohi@gmail.com>
2019-07-07 03:32:28 +0900

    helper profile

---
+++ app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivityC2.java
@@ -0,0 +1,16 @@
+package com.example.appjam_willson.HelperProfileEdit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appjam_willson.R;
+
+public class HelperProfileEditActivityC2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helper_profile_edit_c2);
+    }
+}
