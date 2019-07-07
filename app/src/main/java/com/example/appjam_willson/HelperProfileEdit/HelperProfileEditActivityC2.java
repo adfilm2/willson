5a7db19712197390778935683fa31f330ce7957b
박지영 <batzerohi@gmail.com>
2019-07-07 17:17:55 +0900

    HelperProfileEditActivityC2 Activity추가

--- app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivityC2.java
+++ app/src/main/java/com/example/appjam_willson/HelperProfileEdit/HelperProfileEditActivityC2.java
@@ -1,16 +1,133 @@
-package com.example.appjam_willson.HelperProfileEdit;
+        package com.example.appjam_willson.HelperProfileEdit;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
-import com.example.appjam_willson.R;
+        import com.example.appjam_willson.R;
 
-public class HelperProfileEditActivityC2 extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_helper_profile_edit_c2);
-    }
-}
+
+
+
+        public class HelperProfileEditActivityC2 extends AppCompatActivity {
+        //다음버튼에 액티비티 2 와 연결 해야함
+
+
+            public void onCreate(Bundle savedInstanceState)
+            {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_helper_profile_edit_c2);
+                Button button1 = (Button) findViewById(R.id.helper_editc2_1) ;
+                Button button2 = (Button) findViewById(R.id.helper_editc2_2) ;
+                Button button3 = (Button) findViewById(R.id.helper_editc2_3) ;
+                Button button4 = (Button) findViewById(R.id.helper_editc2_4) ;
+                Button nextbtn = (Button)findViewById(R.id.btn_next);
+
+
+                button1.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nextbtn.setEnabled(true);
+                        button1.setBackgroundResource(R.drawable.helpersignupbackground);
+                        button1.setTextColor(getColor(R.color.white));
+                        button2.setTextColor(getColor(R.color.lightPurple));
+                        button3.setTextColor(getColor(R.color.lightPurple));
+                        button4.setTextColor(getColor(R.color.lightPurple));
+                        button2.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button3.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button4.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+
+
+                    }
+
+
+
+                });
+                button2.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nextbtn.setEnabled(true);
+                        button2.setBackgroundResource(R.drawable.helpersignupbackground);
+                        button2.setTextColor(getColor(R.color.white));
+                        button1.setTextColor(getColor(R.color.lightPurple));
+                        button3.setTextColor(getColor(R.color.lightPurple));
+                        button4.setTextColor(getColor(R.color.lightPurple));
+                        button1.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button3.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button4.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+
+
+
+
+
+
+                    }
+                });
+
+                button3.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nextbtn.setEnabled(true);
+                        button3.setBackgroundResource(R.drawable.helpersignupbackground);
+                        button3.setTextColor(getColor(R.color.white));
+                        button2.setTextColor(getColor(R.color.lightPurple));
+                        button1.setTextColor(getColor(R.color.lightPurple));
+                        button4.setTextColor(getColor(R.color.lightPurple));
+                        button1.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button2.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button4.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+
+
+
+
+                    }
+                });
+
+                button4.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nextbtn.setEnabled(true);
+                        button4.setBackgroundResource(R.drawable.helpersignupbackground);
+                        button4.setTextColor(getColor(R.color.white));
+                        button2.setTextColor(getColor(R.color.lightPurple));
+                        button3.setTextColor(getColor(R.color.lightPurple));
+                        button1.setTextColor(getColor(R.color.lightPurple));
+                        button1.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button2.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+                        button3.setBackgroundResource(R.drawable.helpersignup_nonchecked);
+
+
+                    }
+                });
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+            Button.OnClickListener mClickListener = new View.OnClickListener() {
+                public void onClick(View v) {
+                    //이곳에 버튼 클릭시 일어날 일을 적습니다.
+                }
+            };
+        };
+
+
+
+
+
+
+
