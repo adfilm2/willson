fbfbabe11ef592768d4ea3729f5846c0a30cf553
박지영 <batzerohi@gmail.com>
2019-07-07 17:53:46 +0900

    HelperProfileEditActivityC2 Activity추가

--- app/src/main/java/com/example/appjam_willson/HelperSignUpActivity/helperSignUpActivityStart.java
+++ app/src/main/java/com/example/appjam_willson/HelperSignUpActivity/helperSignUpActivityStart.java
@@ -3,11 +3,22 @@ package com.example.appjam_willson.HelperSignUpActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.appjam_willson.R;
 
 public class helperSignUpActivityStart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helper_sign_up_start);
+
+        Button nextbtn = (Button)findViewById(R.id.HelperSU_btn_next); //버튼이름 변경
+
+
     }
 }
+
