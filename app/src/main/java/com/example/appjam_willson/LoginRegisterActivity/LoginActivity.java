9397de4b275ad442842a98ae081780d111b69fc1
batzerohi <batzerohi@gmail.com>
2019-07-12 00:05:43 +0900

    helpersignup2 activity button 수정

--- app/src/main/java/com/example/appjam_willson/LoginRegisterActivity/LoginActivity.java
+++ app/src/main/java/com/example/appjam_willson/LoginRegisterActivity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.appjam_willson.LoginRegisterActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -80,7 +82,14 @@ public class LoginActivity extends AppCompatActivity {
                 LoginUser(email, password);
             }
         });
+
+
+
+
+
     }
+
+
 
 
     public void LoginUser(String email, String password){
