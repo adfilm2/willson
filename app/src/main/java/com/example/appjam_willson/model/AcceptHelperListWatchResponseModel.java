aa740a8a45c2211dd2ab0828e4cda591c4c418bd
UJeong <pl3434on@gmail.com>
2019-07-12 17:57:49 +0900

    wrong code complete

--- app/src/main/java/com/example/appjam_willson/model/AcceptHelperListWatchResponseModel.java
+++ app/src/main/java/com/example/appjam_willson/model/AcceptHelperListWatchResponseModel.java
@@ -6,7 +6,7 @@ public class AcceptHelperListWatchResponseModel {
 
     public int code;
     public String message;
-    public Data data;
+    public List<Data> data;
 
     /*getter and setter start*/
 
@@ -26,11 +26,11 @@ public class AcceptHelperListWatchResponseModel {
         this.message = message;
     }
 
-    public Data getData() {
+    public List<Data> getData() {
         return data;
     }
 
-    public void setData(Data data) {
+    public void setData(List<Data> data) {
         this.data = data;
     }
 
@@ -39,25 +39,10 @@ public class AcceptHelperListWatchResponseModel {
 
     public static class Data {
         List<Accept_helper> helper;
-        List<Accept_Experience> experience;
+        String[] experience;
 
         /*getter and setter start*/
 
-        public List<Accept_helper> getHelper() {
-            return helper;
-        }
-
-        public void setHelper(List<Accept_helper> helper) {
-            this.helper = helper;
-        }
-
-        public List<Accept_Experience> getExperience() {
-            return experience;
-        }
-
-        public void setExperience(List<Accept_Experience> experience) {
-            this.experience = experience;
-        }
 
         /*getter and setter end*/
 
@@ -68,6 +53,7 @@ public class AcceptHelperListWatchResponseModel {
         String gender;
         String age;
         String category_name;
+        String title;
         String content;
         String stars;
         String review_count;
@@ -75,90 +61,12 @@ public class AcceptHelperListWatchResponseModel {
 
         /*getter and setter start*/
 
-        public String getNickname() {
-            return nickname;
-        }
-
-        public void setNickname(String nickname) {
-            this.nickname = nickname;
-        }
-
-        public String getGender() {
-            return gender;
-        }
-
-        public void setGender(String gender) {
-            this.gender = gender;
-        }
-
-        public String getAge() {
-            return age;
-        }
-
-        public void setAge(String age) {
-            this.age = age;
-        }
-
-        public String getCategory_name() {
-            return category_name;
-        }
-
-        public void setCategory_name(String category_name) {
-            this.category_name = category_name;
-        }
-
-        public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-
-        public String getStars() {
-            return stars;
-        }
-
-        public void setStars(String stars) {
-            this.stars = stars;
-        }
-
-        public String getReview_count() {
-            return review_count;
-        }
-
-        public void setReview_count(String review_count) {
-            this.review_count = review_count;
-        }
-
-        public int getHelper_idx() {
-            return helper_idx;
-        }
-
-        public void setHelper_idx(int helper_idx) {
-            this.helper_idx = helper_idx;
-        }
 
         /*getter and setter end*/
 
     }
 
-    public class Accept_Experience {
-        String[] experience_name;
 
-        /*getter and setter start*/
-
-        public String[] getExperience_name() {
-            return experience_name;
-        }
-
-        public void setExperience_name(String[] experience_name) {
-            this.experience_name = experience_name;
-        }
-
-        /*getter and setter end*/
-
-    }
 
 
 }
